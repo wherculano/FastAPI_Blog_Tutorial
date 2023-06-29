427c9d800471652aefcda06ff94c5847c17eca47
Wagner Herculano <wagherculano@hotmail.com>
2023-06-28 23:52:24 -0300

    Query Parameter example and type hint added

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ app = FastAPI()
 
 
 @app.get("/")
-def index():
-    return {"data":"Hello World!"}
+def index() -> dict[str, str]:
+    return {"data": "Hello World!"}
 
 
 @app.get("/blog/unpublished")
-def unpublished():
+def unpublished() -> dict[str, str]:
     return {"data": "unpublished blogs"}
 
 
 @app.get("/blog/{id}")
-def show(id: int):
+def show(id: int) -> dict[str, str]:
     return {"data": id}
 
 
 @app.get("/blog/{id}/comments")
-def comments(id: int):
+def comments(id: int) -> dict[str, str]:
     return {"data": f"comments from ID {id}"}
 
 
+@app.get("/blog")
+def blog(limit: int = 10, published: bool = False) -> dict[str, str]:
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    return {"data": f"{limit} blogs from the db"}
