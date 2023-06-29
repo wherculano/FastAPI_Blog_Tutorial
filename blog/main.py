431f677d11b66d4fc5086d3e91df3cd929132943
Wagner Herculano <wagherculano@hotmail.com>
2023-06-29 16:04:13 -0300

    Retrieving an user

--- blog/main.py
+++ blog/main.py
@@ -65,7 +65,7 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
     return blogs
 
 
-@app.post("/user", status_code=status.HTTP_201_CREATED)
+@app.post("/user", status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     hashedPassword = Hash.bcrypt(request.password)
     new_user = models.User(name=request.name, email=request.email, password=hashedPassword)
@@ -73,3 +73,11 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user/{id}", response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with ID {id} not found.")
+    return user
