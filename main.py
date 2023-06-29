1e1d8e3b486b80316e309c5f11046daff32dba36
Wagner Herculano <wagherculano@hotmail.com>
2023-06-29 00:20:09 -0300

    Request body and BaseModel example

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
 
 
 @app.get("/")
@@ -29,3 +38,8 @@ def blog(limit: int = 10, published: bool = False) -> dict[str, str]:
     if published:
         return {"data": f"{limit} published blogs from the db"}
     return {"data": f"{limit} blogs from the db"}
+
+
+@app.post("/blog")
+def create_blog(blog: Blog) -> dict[str, str]:
+    return {"data": f"Blog is created with title as '{blog.title}'"}
