397b739c8932a04c93974b6c83612a7d529a5948
Wagner Herculano <wagherculano@hotmail.com>
2023-06-29 11:50:16 -0300

    update endpoint implemented

--- blog/main.py
+++ blog/main.py
@@ -35,6 +35,19 @@ def destroy(id: int, db: Session = Depends(get_db)):
     return f"Blog with id {id} was deleted."
 
 
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found.")
+
+    blog.update(request.dict(exclude_unset=True))
+    db.commit()
+
+    return f"Blog with id {id} was updated."
+
+
 @app.get("/blog", status_code=status.HTTP_200_OK)
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
