17c1e305d644eb47ccc0176c7a1a06379cb12d0f
Wagner Herculano <wagherculano@hotmail.com>
2023-06-29 09:49:03 -0300

    Exception and Status Code implemented

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -26,13 +26,17 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
+@app.get("/blog", status_code=status.HTTP_200_OK)
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
 @app.get("/blog/{id}")
-def show(id: int, db: Session = Depends(get_db)):
+def show(id: int, response: Response, db: Session = Depends(get_db),):
     blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available.")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"error": f"Blog with id {id} is not available."}
     return blogs
