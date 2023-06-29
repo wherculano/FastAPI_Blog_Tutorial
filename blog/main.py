c075c88691d27bed7a0184aa971d81acf5b732aa
Wagner Herculano <wagherculano@hotmail.com>
2023-06-29 09:12:31 -0300

    Models and Tables created

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from . import schemas
+from . import schemas, models
+from .database import engine
 
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
 @app.post("/blog")
 def create(request: schemas.Blog):
